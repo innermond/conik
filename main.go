7f9c19c2c647c1363828e1fd90b1b79eaacf0192
gabriel <innermond@yahoo.com>
2019-08-05 00:38:25 +0300

    doc

--- main.go
+++ main.go
@@ -37,11 +37,9 @@ func main() {
 		t, b = b, t
 	}
 
+	// half of cone angle (in radians)
 	a2 := math.Asin(0.5 * (b - t) / h)
 	a = 2 * a2
-	if a >= 3 || (b-t)*0.5 >= h {
-		log.Fatalf("imposible label having height %.2f for cone with diameters %.2f %.2f/n", h, b, t)
-	}
 	// top circle unwrapped
 	l = math.Pi * t
 	// (p+h2)/(b/2) = p/(t/2)
@@ -50,15 +48,21 @@ func main() {
 	// entire cone radius
 	q := p + h
 
+	// adjust angle regarding requested label width
 	if w > 0.0 {
 		a = w / q
 		a2 = 0.5 * a
 	}
-
-	if g > 0.0 {
+	// adjust angle to take in account a gap betweem ends
+	if g != 0.0 {
 		ag := g / q
 		a -= ag
 		a2 = 0.5 * a
+	}
+
+	// unfolded cone no more than sem-circle - 3 radians
+	if a >= 3 || (b-t)*0.5 >= h {
+		log.Fatalf("imposible label having height %.2f for cone with diameters %.2f %.2f/n", h, b, t)
 	}
 
 	// height of encompassing rect for curved label
@@ -84,16 +88,19 @@ func main() {
 	q2x := math.Sin(a) * q
 	q2y := math.Cos(a) * q
 
+	// reposition for a nice, natural view of label
 	transform := "translate(%f,%f) "
 	rectw := q * a
-	transform = fmt.Sprintf(transform, 0.5*rectw, -1.0*q+hx)
+	transform = fmt.Sprintf(transform, 0.5*rectw, -1.0*(q-hx))
 	if flip {
 		transform = "translate(%f,%f) scale(1,-1) "
-		transform = fmt.Sprintf(transform, q2x, q)
+		transform = fmt.Sprintf(transform, 0.5*rectw, q)
 	}
 	transform += "rotate(%f)"
 	degrees := a2 * 180 / math.Pi
 	transform = fmt.Sprintf(transform, degrees)
+
+	// curved label
 	curve := `
 <path
 	d="M%f %f Q%f %f %f %f L%f %f Q%f %f %f %f Z"
@@ -102,6 +109,19 @@ func main() {
 />`
 	curve = fmt.Sprintf(curve, p2x, p2y, p1x, p1y, px, py, qx, qy, q1x, q1y, q2x, q2y, transform)
 
+	// straightened label
+	rect := ""
+	if d {
+		rect = `
+<rect
+	x="%f" y="%f"
+	width="%f" height="%f"
+	fill="none" stroke="black" stroke-width="0.1"
+/>`
+		rect = fmt.Sprintf(rect, 0.0, 0.0, rectw, h)
+	}
+
+	// final composition
 	svg := `
 <svg
 	width="%f%s" height="%f%s"
@@ -114,16 +134,6 @@ func main() {
 	ox, oy := 0.0, 0.0
 	o1x, o1y := rectw, hx
 	unit := "mm"
-	rect := ""
-	if d {
-		rect = `
-<rect
-	x="%f" y="%f"
-	width="%f" height="%f"
-	fill="none" stroke="black" stroke-width="0.1"
-/>`
-		rect = fmt.Sprintf(rect, 0.0, 0.0, rectw, h)
-	}
 	svg = fmt.Sprintf(
 		svg,
 		o1x, unit,
